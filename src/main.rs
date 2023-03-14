df3084207a0d4724aa8d48b8f974e1b4d4ce0ab6
tenax66 <115792382+tenax66@users.noreply.github.com>
2023-03-14 20:34:04 +0900

    implement writing method for VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -18,12 +18,7 @@ pub extern "C" fn _start() -> ! {
 
     let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
